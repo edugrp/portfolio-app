adda0710311b2370b0fdb83428379175371ee39c
Eduardo Pinto <edugrp@users.noreply.github.com>
2019-10-30 15:08:23 -0400

    Basic header and navigation implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MyWalletComponent } from './my-wallet/my-wallet.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { CryptosComponent } from './cryptos/cryptos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'my-wallet', component: MyWalletComponent},
+  { path: 'stocks', component: StocksComponent},
+  { path: 'cryptos', component: CryptosComponent},
+  { path: '', redirectTo: '/my-wallet', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material-module';
+
+import { HeaderComponent } from './header/header.component';
+import { MyWalletComponent } from './my-wallet/my-wallet.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { CryptosComponent } from './cryptos/cryptos.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MyWalletComponent,
+    StocksComponent,
+    CryptosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
